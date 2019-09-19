ce7a268ef2f328315fd82b4c653603a53e394656
Matěj Žemlička <matejzemlicka@Matej-MacBook.local>
2019-09-19 21:05:08 +0200

    Done

---
+++ Work/js.js
@@ -0,0 +1,16 @@
+for (let i=1; i < 11; i++) {
+    if (i === 1) {
+        document.write(i+" zelená láhev stojí na stole a jedna lahev spadne <br>");
+    }
+
+    if (i < 5 && i > 1) {
+        document.write(i+" zelené láhev stojí na stole a jedna lahev spadne <br>");
+    }
+
+    else {
+        document.write(i+" zelených lahví stojí na stole a jedna lahev spadne <br>");
+    }
+    
+}
+
+
